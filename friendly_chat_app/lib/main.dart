d81e6033a13ec55b077367d4e1d5e45ce1949117
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2019-07-26 16:32:36 -0500

    text composer widget

--- friendly_chat_app/lib/main.dart
+++ friendly_chat_app/lib/main.dart
@@ -13,7 +13,29 @@ class MyFriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +43,7 @@ class ChatScreen extends StatelessWidget {
         title: Text('FriendlyChat App'),
         backgroundColor: Color(0xffD53791),
       ),
+      body: _buildTextComposer(),
     );
   }
 }
